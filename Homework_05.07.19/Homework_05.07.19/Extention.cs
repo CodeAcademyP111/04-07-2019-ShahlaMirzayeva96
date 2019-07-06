ce2903f48508ce776774e5913b499a710fb9b2db
ShahlaMirzayeva96 <49427424+ShahlaMirzayeva96@users.noreply.github.com>
2019-07-06 16:18:45 +0400

    Add files via upload

---
+++ Homework_05.07.19/Homework_05.07.19/Extention.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework_05._07._19
+{
+    public static class Extention
+    {
+
+        public static void SimpleNum(this int number)
+        {
+            int count = 0;
+            for (int i = 1; i <= number; i++)
+            {
+
+                if (number % i == 0)
+                {
+                    count++;
+                }
+
+            }
+
+
+            if (count > 2)
+            {
+                Console.WriteLine("Sade eded deyil");
+            }
+            else
+            {
+                Console.WriteLine("Sade ededdir");
+            }
+
+
+
+        }
+
+        public static int MyLastIndexOf(this string word, char letter)
+        {
+            int index = 1;
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+
+                if (word[i] == letter)
+                {
+                    return index = i + 1;
+
+                }
+
+
+
+
+
+            }
+
+            return index;
+
+        }
+
+
+    }
+}
---
+++ Homework_05.07.19/Homework_05.07.19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework_05._07._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 11;
+            //a.SimpleNum();
+
+
+            string text = "sdedfvfvrtm";
+            char letter = 'e';
+            Console.WriteLine(text.MyLastIndexOf(letter));
+
+        }
+    }
+}
